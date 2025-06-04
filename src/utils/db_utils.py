526658518c7a9f3bb3475b86431918252a3404e5
Matthew Baxter <matthewbaxter0412@hotmail.com>
2025-06-04 11:11:29 -0400

    Adding in EDA for pytorch model

---
+++ src/utils/db_utils.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+
+load_dotenv()
+
+
+class DatabaseConnection:
+    def __init__(self, conn_str: str | None = None):
+        if isinstance(conn_str, type(None)):
+            self.conn_str = os.getenv("DATABASE_URL")
+            print(self.conn_str)
+        else:
+            self.conn_str = conn_str
+
+        self.conn: Engine | None = None
+
+    def __enter__(self) -> Engine:
+        self.engine = create_engine(self.conn_str)
+        return self.engine
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.engine:
+            self.engine.dispose()
