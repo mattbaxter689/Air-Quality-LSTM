28e787ba06b0591759c28bb72fbc2268973d2809
Matt <58673661+mattbaxter689@users.noreply.github.com>
2025-09-27 11:28:25 -0400

    Update init_logger.py
    Updating logging to include file handler

--- src/utils/init_logger.py
+++ src/utils/init_logger.py
@@ -19,19 +19,19 @@ def create_logger(name: str = "torch_weather") -> logging.Logger:
         ch = logging.StreamHandler()
         ch.setLevel(logging.INFO)
 
-        # File handler
-        # fh = logging.FileHandler(f"logs/{name}.log")
-        # fh.setLevel(logging.INFO)
+        File handler
+        fh = logging.FileHandler(f"logs/{name}.log")
+        fh.setLevel(logging.INFO)
 
         # Formatter
         formatter = logging.Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         )
         ch.setFormatter(formatter)
-        # fh.setFormatter(formatter)
+        fh.setFormatter(formatter)
 
         # Add handlers
         logger.addHandler(ch)
-        # logger.addHandler(fh)
+        logger.addHandler(fh)
 
     return logger
